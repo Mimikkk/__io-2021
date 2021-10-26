134fb878c1e0b98ca9581fd613da2c05ac769085
Hououin Kyouma <inwazzin@gmail.com>
2021-10-26 08:59:19 +0200

    how raddicalll

--- 04-Git/Program.java
+++ 04-Git/Program.java
@@ -2,8 +2,12 @@ public class Program {
     private static sayHello() {
         System.out.println("How Fun!");
     }
+    private static sayHello() {
+        System.out.println("Very rad...");
+    }
 
     public static void main(String[] args) {
         sayHello();
+        howRad();
     }
 }
